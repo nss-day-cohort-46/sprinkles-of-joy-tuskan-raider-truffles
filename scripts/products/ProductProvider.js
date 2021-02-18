1e58f3074a463b516523a5e3097eb0df564fc03c
Stacey Littrell <stacey.littrell@gmail.com>
2021-02-18 17:26:45 -0600

    fixed product rendering

--- scripts/products/ProductProvider.js
+++ scripts/products/ProductProvider.js
@@ -2,9 +2,8 @@ import { bakeryAPI } from "../Settings.js"
 
 let products = []
 
-export const useProducts = () => {
-  products.slice()
-}
+export const useProducts = () => products.slice()
+
 
 export const getProducts = () => {
   return fetch(`${bakeryAPI.baseURL}/products`)
