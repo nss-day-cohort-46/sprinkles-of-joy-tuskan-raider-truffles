6b0d59b6080fdd18561d9268986f13e584cae2f7
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-02-23 15:01:18 -0600

    Logout updated/ Order History still not working

--- scripts/orders/OrderList.js
+++ scripts/orders/OrderList.js
@@ -11,46 +11,58 @@ const eventHub = document.querySelector("#container")
 const contentContainer = document.querySelector(".userOrders")
 
 let customerOrders = []
-let customerProduts = []
+let customerProducts = []
 let customerOP = []
 
 export const OrderList = () => {
   // if (authHelper.isUserLoggedIn()) {
-  if (authHelper.getCurrentUserId()) {
+  if (authHelper.isUserLoggedIn()) {
      (getOrders())
       .then(getProducts)
       .then(getOrderProducts)
       .then(() => {
         customerOrders = useOrders()
-        customerProduts = useProducts()
+        customerProducts = useProducts()
         customerOP = useOrderProducts()
-        debugger
-        customerOrders = customerOrders.filter(order =>order.id === authHelper.getCurrentUserId())
-        console.log("Yum", customerOrders)
-        // CustomerOrderHistory = customerOrders1.map()
-        render()
+        // debugger
+        customerOrders = customerOrders.filter(order =>order.customerId === parseInt(authHelper.getCurrentUserId()))
+        // console.log("Yum", customerOrders)
+        const CustomerOrderHistory = customerOrders.map(orders => {
+          let orderProductArray = customerOP.filter(orderProd => orderProd.orderId === orders.id)
+           filteredProd = orderProductArray.map(opa => {
+            return customerProducts.find(products => products.id === opa.productId)
+          })
+          return Order(orders, filteredProd)
+        })
+        render(CustomerOrderHistory)
       })
 
       }
   }
 
-// const renderToDom = (orderCollection) => {
+// STEP BY STEP ORDER HISTORY
+// Get information from orders
+// Get order products (Line 22)
+// Get products for product name (Line 21)
+// Get current use ID (Line 28)
+// Get customer orders and then filter for order.id to currentUserID (Line 28)
+  // const renderToDom = (orderCollection) => {
   
 //   for (const order of orderCollection) {
 //   const orderRelationship = orderProductsJoinTable.filter(orderProducts => orders.customerId === customerOrders.customerId)
 //   }
 // }
 
-const render = () => {
-  const ordersHtmlRepresentation = customerOrders.map(order => Order(order)).join("")
+const render = (cusOrderHistory) => {
+  // const ordersHtmlRepresentation = customerOrders.map(order => Order(order)).join("")
 
-  contentContainer.innerHTML = `
+  contentContainer.innerHTML = `CustomerOrderHistory
   <div id="orders__modal" class="modal--parent">
         <div class="modal--content">
         <h3>Previous Orders</h3>
         <div>
         <h5>Ordered on</h5>
-        ${ordersHtmlRepresentation}
+        ${cusOrderHistory.statusId}
         </div>
         <button id="modal--close">Close</button>
         </div>
